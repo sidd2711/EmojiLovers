494337923a8de9e2fc2e73026e31a5fb3581e152
Siddharth Charkhawala <sid.charkhawala@gmail.com>
2024-02-04 21:51:49 +0530

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Siddharth Charkhawala on 04/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EmojiLoverApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ EmojiLover/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  EmojiLover
+//
+//  Created by Siddharth Charkhawala on 04/02/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
